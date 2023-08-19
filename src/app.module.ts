30de60827deed9784947ff9d7561bdf32b9446bd
mangel1196 <mangel1196@hotmail.com>
2023-08-18 22:11:33 -0500

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ContainerController } from './controller/container.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ContainerController],
   providers: [AppService],
 })
 export class AppModule {}
